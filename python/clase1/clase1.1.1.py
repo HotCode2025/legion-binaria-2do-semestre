b46285deb614df11f842e918a42134833714384e
joelcm20 <132691926+joelcm20@users.noreply.github.com>
2025-08-27 18:40:32 -0300

    se agrega ejercicio 1.1.1 de python

---
+++ python/clase1/clase1.1.1.py
@@ -0,0 +1,8 @@
+# list = cualquier tipo(str, int, float, boolean). Tambien puedes combinarlos
+# Ej: ["hola", 4, 5.6, True]
+
+nombres = ["Joel", "Cristian", "Lucia", "Sebastian", "Johana", "Jackeline"]
+print(nombres) # Imprime la lista completa
+print(nombres[0]) # Imprime el primer elemento de la lista
+print(nombres[3]) # Imprime el cuarto elemento de la lista
+print(nombres[-1]) # Imprime el ultimo elemento de la lista. Los numeros negativos comienzan desde el final
