0e7040d7efe7897dd91589b4df6b0dd2797e047a
jackelinesoledadrodriguez <jackelinesoledadrodriguez@gmail.com>
2025-08-31 16:37:07 -0300

    Se agrega Ejemplo 2.4 de Python

---
+++ python/clase2/2.4Repaso_y_mas_conceptos_de_Tuplas.py
@@ -0,0 +1,11 @@
+'''
+TUPLAS
+'''
+
+# Repasos de Tuplas
+tupla = (4,'Hola',6.78,[1,2,78],4,'HOLA')
+print(tupla)
+
+print(4 in tupla) # Acción booleana, su respuesta es de tipo booleana
+# Lo que podemos usar dentro de tuplas son: index, count, len
+# En tuplas se puede convertir de tupla a lista y viceversa
