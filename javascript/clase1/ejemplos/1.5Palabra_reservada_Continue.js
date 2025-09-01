b8e604d1d24c2e32d955dc3da229aca5857f7d67
johana <johaelila@gmail.com>
2025-09-01 17:49:28 -0300

    Se agrega ejemplo 1.5 palabra reservada Continue de JavaScript

---
+++ javascript/clase1/ejemplos/1.5Palabra_reservada_Continue.js
@@ -0,0 +1,11 @@
+
+// Palabra reservada continue
+for(let contador = 0; contador < 10; contador++ ){
+    if(contador % 2 != 0){
+        continue; // ir a la siguiente iteracion
+    }
+    console.log(contador); // Muestra todos los pares
+}   
+console.log("Termina el ciclo al encontrar los pares");
+
+
