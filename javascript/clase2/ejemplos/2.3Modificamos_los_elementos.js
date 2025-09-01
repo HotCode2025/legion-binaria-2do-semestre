6aba9e5469cabba71ae3754d021da041db8fa675
johana <johaelila@gmail.com>
2025-09-01 18:33:09 -0300

    Se agrega ejemplo 2.3 modificacion de elementos de JavaScript

---
+++ javascript/clase2/ejemplos/2.3Modificamos_los_elementos.js
@@ -0,0 +1,15 @@
+// Modificamos los elemtnos del arreglo
+autos [1] = 'Volvo';
+console.log(autos[1]);
+
+// Agregamos nuevos valores al arreglo
+autos.push('Audi');
+console.log(autos);
+
+// Otras formas de agregar elementos al arreglo
+autos[autos.length] = 'Porsche';
+console.log(autos);
+
+// Tercera forma de agreagar un elementos teniendo CUIDADO
+//autos[6] = 'Ranault';
+//console.log(autos);
