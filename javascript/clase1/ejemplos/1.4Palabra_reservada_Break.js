750481a96e5ad934a41d003bfffb2c3c9d1cf69c
Criss001 <cristianjfarina@gmail.com>
2025-09-01 16:52:11 -0300

    Agregue ejemplo 1.4 palabra reservada en JavaScript

---
+++ javascript/clase1/ejemplos/1.4Palabra_reservada_Break.js
@@ -0,0 +1,9 @@
+
+// Palabra reservada break
+for(let contador = 0; contador < 10; contador++ ){
+    if(contador % 2 == 0){
+        console.log(contador); // Muestra todos los pares
+        break;
+    }
+}
+console.log("Termina el ciclo al encontrar los pares");
