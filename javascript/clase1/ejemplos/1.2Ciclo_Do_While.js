312957a05899106b5fc27fbfde790e2e32557566
Sebastian <sebaguzowski@gmail.com>
2025-09-02 13:27:38 -0300

    agrego ejercicio ciclo do-while de la clase 1, javascript

---
+++ javascript/clase1/ejemplos/1.2Ciclo_Do_While.js
@@ -0,0 +1,16 @@
+/* While */
+/*
+let contador = 0;
+while (contador < 3){
+    console.log(contador);
+    contador++;
+}
+console.log("Fin del ciclo while");
+*/
+/* Do While */
+let conteo = 0;
+do{
+    console.log(conteo);
+    conteo++;    
+}while(conteo < 3);
+console.log("Fin del ciclo do while");
