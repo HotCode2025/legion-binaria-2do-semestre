d766135c89109683dd1c59e93d01739eeecc18e0
joelcm20 <132691926+joelcm20@users.noreply.github.com>
2025-09-02 19:40:44 -0300

    se agrega ejemplo 4.5 de javascript

---
+++ javascript/clase4/ejemplos/4.5AgregarYEliminarPropiedadesObjetos.js
@@ -0,0 +1,33 @@
+const persona = {
+  nombre: "Luis",
+  apellido: "Perez",
+  edad: 23,
+  direccion: "Calle 123",
+  telefono: "123456789",
+};
+
+// cambiamos dinamicamente el valor de una propiedad del objeto
+persona.nombre = "Pedro";
+
+// nota: Si al asignarle un nuevo valor a una propiedad que no existe, se creara una nueva propiedad con el nuevo valor
+persona.nombres = "Luis";
+console.log(persona);
+// {
+//   nombre: "Pedro",
+//   apellido: "Perez",
+//   edad: 23,
+//   direccion: "Calle 123",
+//   telefono: "123456789",
+//   nombres: "Luis" <------- Nueva propiedad
+// };
+
+// eliminar una propiedad
+delete persona.nombres; // Se elimina la propiedad 'nombres'
+console.log(persona);
+// {
+//   nombre: "Pedro",
+//   apellido: "Perez",
+//   edad: 23,
+//   direccion: "Calle 123",
+//   telefono: "123456789"
+// };
