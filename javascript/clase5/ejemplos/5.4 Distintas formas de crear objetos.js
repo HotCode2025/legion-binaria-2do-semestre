f0a06c64194fb95c742b1be30d48c5c8e11c171d
fr4ncisx <fr4ncisx@gmail.com>
2025-09-14 00:24:31 -0300

    Se suben ejemplos 5.4 y 5.5 de JavaScript

---
+++ javascript/clase5/ejemplos/5.4 Distintas formas de crear objetos.js
@@ -0,0 +1,30 @@
+// Diferentes formas de crear objetos
+// caso objeto 1
+let miObjeto = new Object() //Esta es una opción formal
+// caso objeto 2
+let miObjeto2 = {} //Esta opción es breve y recomendada
+
+//caso String 1
+let miCadena = new String('Hola') //Sintaxis formal
+//caso String 2
+let miCadena2 = 'Hola' //Esta es la sintaxis simplificada y recomendada
+
+//caso con números 1
+let miNumero = new Number(1) //Es formal no recomendable
+//caso con números 2
+let miNumero1 = 1 // Sintaxis recomendada
+
+//caso boolean 1
+let miBoolean = new Boolean(false)// Formal
+//caso boolean 2
+let miBoolean1 = false //Sintaxis recomendada
+
+//caso Arreglos 1
+let miArreglo1 = new Array() //Formal
+//caso Arreglos 1
+let miArreglo2 = [] //Sintaxis recomendada
+
+// caso function 1
+let miFuncion1 = new function(){} // todo despues de new es considerado objeto
+// caso function 2
+let miFuncion2 =  function(){} // notación simplificada y recomendada
