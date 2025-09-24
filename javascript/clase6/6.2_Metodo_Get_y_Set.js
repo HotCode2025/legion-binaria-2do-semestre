96e494297cbb8432cd34d8d8ae96f0896122531d
Criss001 <cristianjfarina@gmail.com>
2025-09-23 21:50:46 -0300

    Agregue Ejemplo 6.1;6.2;6.3 clase 6 JavaScript

---
+++ javascript/clase6/6.2_Metodo_Get_y_Set.js
@@ -0,0 +1,39 @@
+class Persona {
+    constructor(nombre, apellido) {
+        this._nombre = nombre;
+        this._apellido = apellido;
+    }
+
+    get nombre() {
+        return this._nombre;
+    }
+
+    set nombre(nombre) {
+        this._nombre = nombre;
+    }
+
+     get apellido() {
+        return this._apellido;
+    }
+
+    set apellido(apellido) {
+        this._apellido = apellido;
+    }
+}
+
+let persona1 = new Persona('Martín', 'Perez');
+console.log(persona1.nombre);
+// console.log(persona1);
+
+let persona2 = new Persona('Carlos', 'Lara');
+console.log(persona2.nombre); 
+// console.log(persona2);
+
+let persona3 = new Persona('Martín', 'Perez');
+console.log(persona1.apellido);
+
+// Modificamos el apellido usando el setter
+persona1.apellido = 'Gómez';
+
+// Mostramos el nuevo valor usando el getter
+console.log(persona1.apellido);
