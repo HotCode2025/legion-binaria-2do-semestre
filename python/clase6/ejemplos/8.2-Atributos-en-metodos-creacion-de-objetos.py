87856a897f05fc51e99cdfc6d02a4880218ab55a
Sebastian <sebaguzowski@gmail.com>
2025-09-29 09:24:42 -0300

    agrego ejercicios 8.1 y 8.2 de la clase 6, python

---
+++ python/clase6/ejemplos/8.2-Atributos-en-metodos-creacion-de-objetos.py
@@ -0,0 +1,9 @@
+class Persona:
+    def __init__(self):
+        self.nombre = "Juan"
+        self.apellido = "Zalazar"
+        self.edad = 22
+persona1 = Persona()
+print(persona1.nombre)
+print(persona1.apellido)
+print(persona1.edad)
