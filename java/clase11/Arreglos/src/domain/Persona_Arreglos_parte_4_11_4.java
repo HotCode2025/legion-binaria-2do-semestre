19366e3ec00845588c6b27b117bf60de186c1acb
johana <johaelila@gmail.com>
2025-11-10 17:31:21 -0300

    Se agregan clase 10 y 11 de Java

---
+++ java/clase11/Arreglos/src/domain/Persona_Arreglos_parte_4_11_4.java
@@ -0,0 +1,20 @@
+
+package domain;
+
+public class Persona_Arreglos_parte_4_11_4 {
+    private String nombre;
+
+    public Persona_Arreglos_parte_4_11_4(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    
+}
---
+++ java/clase11/Matrices/src/test/TestMatrices_12_1_Manejo_de_Matrices_Parte_1_Aquí_creamos_la_carpeta_Leccion10.java
@@ -0,0 +1,6 @@
+
+package test;
+
+public class TestMatrices_12_1_Manejo_de_Matrices_Parte_1_Aquí_creamos_la_carpeta_Leccion10 {
+    
+}
